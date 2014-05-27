ee077184b159039f158aa8ce4ab20daee80a6726
justus scheder-bieschin <justusscheder-bieschin@justuss-MacBook-Air.local>
2014-05-27 10:50:33 +0200

    fizzbuzz.rb

---
+++ bouncer.rb
@@ -0,0 +1,9 @@
+puts "How old are you?"
+
+age = gets.to_i 
+
+if age >= 21
+	puts "Come in" 
+else
+	puts "too young"
+end
---
+++ fizzbuzz.rb
@@ -0,0 +1,20 @@
+x = 1
+
+while x <= 100
+
+if (x %3 == 0)
+	puts "fizz"
+elsif (number%5 == 0)
+	puts "buzz"	
+	
+else 
+	puts x
+
+end
+
+	puts x 
+	x = x + 1
+
+
+end
+
